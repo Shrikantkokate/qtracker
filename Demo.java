fab8514ae5529c3f93f6a1171646905f26f2537e
Shrikantkokate <132645487+Shrikantkokate@users.noreply.github.com>
2024-04-27 09:51:31 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,8 @@
+class Demo
+{
+	public static void main(String[] args) 
+	{
+		int _ = 1_1__4 ;
+		System.out.println($);
+	}
+}
---
+++ login.js
@@ -0,0 +1,11 @@
+
+function checkCred() {
+
+	let cnd = true;
+	if(cnd)
+	{
+		open("homepage.html");
+	}else{
+		alert("Entered Credentials are incorrect")
+	}
+}
